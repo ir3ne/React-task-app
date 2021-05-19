fc18abb1cef75a771fe675055366c2475a147cfb
Irene <irene.tomaini@docplanner.com>
2021-05-19 17:00:54 +0200

    task app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
 import './App.css';
+import uniqid from 'uniqid';
+import React, { Component } from 'react';
+import Overview from './components/Overview';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task: {
+        text: '',
+        id: uniqid(),
+        number: 0
+      },
+      tasks: []
+    }
+
+    this.addTask = this.addTask.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.handleTask = this.handleTask.bind(this); 
+    this.editTask = this.editTask.bind(this);
+    this.changeInput = this.changeInput.bind(this);
+  }
+
+  handleInput(e) {
+    this.setState({
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+        number: this.state.task.number,
+        editable: false
+      }
+    });
+  }
+
+  changeInput(e) {
+    const editedInput = e.target.value;
+    const taskId = e.currentTarget.dataset.id;
+    this.setState({
+      tasks: this.state.tasks.map(item => {
+        if(taskId === item.id) {
+          item.text = editedInput
+        }
+        return item;
+      })
+    });
+  }
+
+  addTask() {
+    this.setState({
+      task: {
+        text: '',
+        id: uniqid(),
+        number: this.state.task.number + 1
+      },
+      tasks: [...this.state.tasks, this.state.task]
+    });
+  }
+
+  handleTask(e) {
+    const taskId = e.currentTarget.dataset.id;
+    this.setState({
+      tasks: this.state.tasks.filter(task => task.id !== taskId)
+    })
+  }
+
+  editTask(e) {
+    const taskId = e.currentTarget.dataset.id;
+    const taskToEdit = this.state.tasks.filter(task => task.id === taskId);
+    if (!taskToEdit.editable) {
+      this.setState({
+        tasks: this.state.tasks.map(item => {
+          if(taskId === item.id) {
+            item.editable = !item.editable
+          }
+          return item;
+        })
+      });
+    }
+    
+    console.log('test', this.state.tasks);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input onChange={this.handleInput} placeholder="Write new task" value={this.state.task.text} type="text" />
+        <button onClick={this.addTask}>Add Task</button>
+        <Overview tasks={this.state.tasks} editTask={this.editTask} handleTask={this.handleTask} changeInput={this.changeInput} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Overview.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Overview extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Tasks list</h2>
+        <ul>
+          {this.props.tasks.map((task, i) => {
+            return (
+              <div style={{ display: 'flex', margin: '6px 0' }}>
+                <li key={i} id={task.id}>
+                  {task.number}. 
+                  <input type="text" defaultValue={task.text} data-id={task.id} onChange={this.props.changeInput} readOnly={!task.editable ? 'readOnly' : null} />
+                </li>
+                <button onClick={this.props.editTask} data-id={task.id} style={{ marginLeft: '20px' }}>{!task.editable ? 'edit' : 'done'}</button>
+                <button onClick={this.props.handleTask} data-id={task.id} style={{ marginLeft: '20px' }}>delete</button>
+              </div>
+            )
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Overview;
